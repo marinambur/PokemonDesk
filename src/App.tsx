825448d4afff695252502c0795dbaa93b3c5054a
marinambur <marinaburykina@gmail.com>
2021-05-12 17:58:43 +0300

    Add ESLint and Prettier

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import cn from 'classnames';
+import s from './App.module.scss';
+
+const App = () => {
+  return <div className={cn(s.header, 'color')}>This is homework-1!</div>;
+};
+
+export default App;
