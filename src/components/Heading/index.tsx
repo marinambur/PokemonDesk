5d8888a104c7444ae490d484a5f9a470a7aba5a4
marinambur <marinaburykina@gmail.com>
2021-05-29 13:36:43 +0300

    homework-6

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -4,11 +4,13 @@ import s from './Heading.module.scss';
 
 interface HeadingProps {
   priority?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 'p';
+  className?: string;
 }
-const Heading: React.FC<HeadingProps> = ({ children, priority }) => {
+const Heading: React.FC<HeadingProps> = ({ children, priority , className}) => {
   const CustomTag = priority !== 'p' ? (`h${priority}` as keyof JSX.IntrinsicElements) : `p`;
   const classes = classNames(
     s.root,
+    className,
     priority !== 'p' ? s[`h${priority}` as keyof typeof s] : s[`${priority}` as keyof typeof s],
   );
   return <CustomTag className={classes}>{children}</CustomTag>;
